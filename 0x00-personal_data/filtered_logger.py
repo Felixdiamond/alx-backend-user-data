5c1f0cc9fb9cc99c6cbca216322d2d0b62930e75
Felixdiamond <diamondfelix006@gmail.com>
2023-08-02 22:38:40 -0700

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,3 +22,33 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{field}=[^{separator}]*',
                          f'{field}={redaction}', message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes RedactingFormatter with given fields
+
+        Args:
+            fields (List[str]): List of fields to obfuscate
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records
+
+        Args:
+            record (logging.LogRecord): Log record
+
+        Returns:
+            str: Formatted log record
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
+
