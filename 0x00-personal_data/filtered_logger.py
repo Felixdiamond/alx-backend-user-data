97b7df5ff3e8547a62d6ae0c4ccba0f94d815403
Felixdiamond <diamondfelix006@gmail.com>
2023-08-02 22:45:19 -0700

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,7 +18,7 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
-        ) -> str:
+) -> str:
     """Filters a log line."""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
