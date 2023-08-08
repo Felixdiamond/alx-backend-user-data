a645924b80e9f796bd9277117059dd139742573c
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-08-08 23:57:16 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,16 +14,29 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if authentication is required for a given path.
-
+    
         Args:
             path (str): The path to check.
-            excluded_paths (List[str]): A list of paths
-            that do not require authentication.
-
+            excluded_paths (List[str]): A list of paths that do not require authentication.
+    
         Returns:
             bool: True if authentication is required, False otherwise.
         """
-        return False
+        if path is None:
+            return True
+    
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+    
+        # Normalize the path and excluded_paths to ensure they are slash tolerant
+        path = path.rstrip('/')
+        excluded_paths = [p.rstrip('/') for p in excluded_paths]
+    
+        if path in excluded_paths:
+            return False
+    
+        return True
+
 
     def authorization_header(self, request=request) -> str:
         """Get the value of the authorization header.
