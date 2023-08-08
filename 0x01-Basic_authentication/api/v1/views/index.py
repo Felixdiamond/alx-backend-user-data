9f7264a7db246bc36355035acd41baa6021fe00c
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-08-08 23:11:42 +0100

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized')
+@app_views.route('/unauthorized')
 def unauthorized():
     """ Endpoint that raises a 401 error.
 
