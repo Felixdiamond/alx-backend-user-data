09bda42105af94f2f52beaa364e20d1973d76de3
Felixdiamond <diamondfelix006@gmail.com>
2023-08-08 16:22:13 -0700

    basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -43,6 +43,31 @@ if os.getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
 
 
+@app.before_request
+def before_request():
+    """Filter requests before they are processed.
+
+    This method checks if authentication is required
+    or the current request and raises an error if necessary.
+    """
+    if auth is None:
+        return
+
+    excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+            ]
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
