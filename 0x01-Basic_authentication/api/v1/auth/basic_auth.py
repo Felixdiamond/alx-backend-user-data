0c2609da3f61fa0d863366ae417f33d75695a0f5
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-08-09 01:37:43 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -115,10 +115,10 @@ class BasicAuth(Auth):
         authorization_header = self.authorization_header(request)
         base64_authorization_header = self.extract_base64_authorization_header(
             authorization_header)
-        decoded_base64_authorization_header =
+        decoded_base64_authorization_header = (
         self.decode_base64_authorization_header(
             base64_authorization_header
-        )
+        ))
         user_email, user_pwd = self.extract_user_credentials(
             decoded_base64_authorization_header)
         user = self.user_object_from_credentials(user_email, user_pwd)
