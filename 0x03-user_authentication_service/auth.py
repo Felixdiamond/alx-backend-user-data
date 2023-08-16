41783a5cd44bcb9c63452cfc90c79ea536018c74
Felixdiamond <diamondfelix006@gmail.com>
2023-08-16 20:31:08 +0100

    user auth service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,9 +3,29 @@
 Auth module
 """
 
+from db import DB
+
 import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password."""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
