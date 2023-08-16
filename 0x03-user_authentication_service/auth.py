87bc91714beed8bafbbded73069ae791b22cfda8
Felixdiamond <diamondfelix006@gmail.com>
2023-08-16 20:25:31 +0100

    user auth service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,13 +2,10 @@
 """
 Auth module
 """
+
 import bcrypt
 
 
-class Auth:
-    """Auth class"""
-
-    @staticmethod
-    def _hash_password(password: str) -> bytes:
-        """Hash a password"""
-        return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+def _hash_password(password: str) -> bytes:
+    """Hashes a password."""
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
